b7a73834825a16be167cef15630170f665bf905e
oriash93 <oriash93@gmail.com>
2016-12-24 18:30:06 +0200

    First commit - created the repository

---
+++ AutoClicker/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AutoClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Dependency Properties
+
+        #region Hours
+
+        public int Hours
+        {
+            get { return (int)GetValue(HoursProperty); }
+            set { SetValue(HoursProperty, value); }
+        }
+
+        public static readonly DependencyProperty HoursProperty =
+            DependencyProperty.Register("Hours", typeof(int), typeof(MainWindow),
+                new PropertyMetadata(0));
+
+        #endregion Hours
+
+        #region Minutes
+
+        public int Minutes
+        {
+            get { return (int)GetValue(MinutesProperty); }
+            set { SetValue(MinutesProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register("Minutes", typeof(int), typeof(MainWindow),
+                new PropertyMetadata(0));
+
+        #endregion Minutes
+
+        #region Seconds
+
+        public int Seconds
+        {
+            get { return (int)GetValue(SecondsProperty); }
+            set { SetValue(SecondsProperty, value); }
+        }
+
+        public static readonly DependencyProperty SecondsProperty =
+            DependencyProperty.Register("Seconds", typeof(int), typeof(MainWindow),
+                new PropertyMetadata(0));
+
+        #endregion Seconds
+
+        #region Milliseconds
+
+        public int Milliseconds
+        {
+            get { return (int)GetValue(MillisecondsProperty); }
+            set { SetValue(MillisecondsProperty, value); }
+        }
+
+        public static readonly DependencyProperty MillisecondsProperty =
+            DependencyProperty.Register("Milliseconds", typeof(int), typeof(MainWindow),
+                new PropertyMetadata(100));
+
+        #endregion Milliseconds
+
+        #region SelectedMouseButton
+
+        public MouseButton SelectedMouseButton
+        {
+            get { return (MouseButton)GetValue(SelectedMouseButtonProperty); }
+            set { SetValue(SelectedMouseButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedMouseButtonProperty =
+            DependencyProperty.Register("SelectedMouseButton", typeof(MouseButton), typeof(MainWindow),
+                new PropertyMetadata(default(MouseButton)));
+
+        #endregion SelectedMouseButton
+
+        #endregion Dependency Properties
+
+        #region Fields
+
+        public List<string> MouseButtons
+        {
+            get { return new List<string> { "Left", "Right" }; }
+            set { }
+        }
+
+        private Timer clickTimer;
+
+        #region Mouse Consts
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        #endregion Mouse Consts
+
+        #endregion Fields
+
+        public MainWindow()
+        {
+            clickTimer = new Timer(CalculateInterval());
+            clickTimer.Elapsed += ClickTimer_Elapsed;
+
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        #region External Methods
+
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        #endregion External Methods
+
+        #region Events
+
+        private void ClickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            int xPosition = 300;
+            int yPosition = 300;
+            int duration = 0;
+
+            ClickMouse(SelectedMouseButton, xPosition, yPosition, duration);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clickTimer.Start();
+        }
+
+        #endregion Events
+
+        #region Helper Methods
+
+        private void ClickMouse(MouseButton selectedMouseButton, int xPosition, int yPosition, int duration)
+        {
+            if (selectedMouseButton == MouseButton.Left)
+                LeftMouseClick(xPosition, yPosition);
+            else
+                RightMouseClick(xPosition, yPosition);
+        }
+
+        //This simulates a left mouse click
+        private static void LeftMouseClick(int xpos, int ypos)
+        {            
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            //System.Threading.Thread.Sleep(5000);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        //This simulates a right mouse click
+        private static void RightMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, xpos, ypos, 0, 0);
+            //System.Threading.Thread.Sleep(5000);
+            mouse_event(MOUSEEVENTF_RIGHTUP, xpos, ypos, 0, 0);
+        }
+
+        private int CalculateInterval()
+        {
+            return Milliseconds + Seconds * 1000 + Minutes * 60 * 1000 + Hours * 3600 * 1000;
+        }
+
+
+        #endregion Helper Methods
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right)
+                Console.WriteLine(e.RightButton);
+            else if (e.ChangedButton == MouseButton.Left)
+                Console.WriteLine(e.LeftButton);
+        }
+    }
+}
