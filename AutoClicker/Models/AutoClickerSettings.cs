0e82dbdbd3c9a94bf1b9781c732fb080fe9c848d
Brandon Shaver <brandon.shaver@goilluminate.com>
2022-02-21 19:27:13 -0600

    Bug Fix / Fix Randomized Interval Textbox Disabling
    I'm not sure why the PropertyChanged trigger was not behaving correctly but by explicitly calling \OnPropertyChanged, I have was able to get the textboxes to respond appropriately.

--- AutoClicker/Models/AutoClickerSettings.cs
+++ AutoClicker/Models/AutoClickerSettings.cs
@@ -1,8 +1,11 @@
-﻿using AutoClicker.Enums;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AutoClicker.Annotations;
+using AutoClicker.Enums;
 
 namespace AutoClicker.Models
 {
-    public class AutoClickerSettings
+    public class AutoClickerSettings : INotifyPropertyChanged
     {
         public int Hours { get; set; }
 
@@ -28,8 +31,6 @@ namespace AutoClicker.Models
 
         public int MinimumMilliseconds { get; set; }
 
-        public bool IsRandomizedIntervalEnabled { get; set; }
-
         public MouseButton SelectedMouseButton { get; set; }
 
         public MouseAction SelectedMouseAction { get; set; }
@@ -43,5 +44,27 @@ namespace AutoClicker.Models
         public int PickedYValue { get; set; }
 
         public int SelectedTimesToRepeat { get; set; }
+
+        private bool _isRandomizedIntervalEnabled = false;
+        public bool IsRandomizedIntervalEnabled
+        {
+            get => _isRandomizedIntervalEnabled;
+            set
+            {
+                _isRandomizedIntervalEnabled = value;
+                OnPropertyChanged(nameof(IsRandomizedIntervalEnabled));
+            }
+        }
+
+
+
+        // The functions below are required to explicitly call the UpdateSourceTrigger for IsRandomizedIntervalEnabled
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
